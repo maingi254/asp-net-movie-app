3759cfb4b42db63f59415d71c382f28537182de9
maingi254 <itskelvinmaingi@gmail.com>
2019-07-20 11:26:39 +0300

    first

---
+++ Data/movieshopContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace movieshop.Models
+{
+    public class movieshopContext : DbContext
+    {
+        public movieshopContext (DbContextOptions<movieshopContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<movieshop.Models.Movie> Movie { get; set; }
+    }
+}
